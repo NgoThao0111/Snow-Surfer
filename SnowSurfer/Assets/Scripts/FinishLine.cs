3ea58011fbdb9c9b10b2c357cdb0fadcc8aad9bd
NgoThao0111 <ngothao0111nt@gmail.com>
2025-08-22 22:48:51 +0700

    Adjust player movement and add restart feature

--- SnowSurfer/Assets/Scripts/FinishLine.cs
+++ SnowSurfer/Assets/Scripts/FinishLine.cs
@@ -1,14 +1,15 @@
-using Unity.VisualScripting;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class FinishLine : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D collision)
     {
-        int layerIndex = LayerMask.NameToLayer("Player");
+        int layerIndex = LayerMask.NameToLayer("Player"); // avoid error when changing layer's index
         if (collision.gameObject.layer == layerIndex)
         {
-            Debug.Log("The player has won");
+            Debug.Log("The player has won"); 
+            SceneManager.LoadScene(0); // restart the game when player meet finish line
         }
     }
 }
