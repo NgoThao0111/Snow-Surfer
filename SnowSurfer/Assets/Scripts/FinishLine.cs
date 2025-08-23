1513233ae63eab4c8b1a0936b12d70b889ed3512
NgoThao0111 <ngothao0111nt@gmail.com>
2025-08-23 21:27:33 +0700

    adjust player movement and level layout for more difficulty

--- SnowSurfer/Assets/Scripts/FinishLine.cs
+++ SnowSurfer/Assets/Scripts/FinishLine.cs
@@ -3,13 +3,19 @@ using UnityEngine;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float restartDelay = 1f;
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Player"); // avoid error when changing layer's index
         if (collision.gameObject.layer == layerIndex)
         {
-            Debug.Log("The player has won"); 
-            SceneManager.LoadScene(0); // restart the game when player meet finish line
+            Debug.Log("The player has won");
+            Invoke("Restart", restartDelay); // restart the game when player meet finish line
         }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(0);
     }
 }
