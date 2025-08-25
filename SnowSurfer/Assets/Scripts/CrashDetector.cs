ad8b916dc6456bc21185a4618d650bba478d18f9
NgoThao0111 <ngothao0111nt@gmail.com>
2025-08-25 15:42:22 +0700

    add particle & environment & adjust layout
    add particle when crash and finish
    add environment objects
    adjust layout and player movement to be more challenging

--- SnowSurfer/Assets/Scripts/CrashDetector.cs
+++ SnowSurfer/Assets/Scripts/CrashDetector.cs
@@ -3,13 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] ParticleSystem crashParticle;
+    [SerializeField] float restartDelay = 1f;
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Floor");
         if (collision.gameObject.layer == layerIndex)
         {
             Debug.Log("The player has lost");
-            SceneManager.LoadScene(0);
+            crashParticle.Play();
+            Invoke("Restart", restartDelay);
         }
+    }
+    void Restart()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- SnowSurfer/Assets/Scripts/FinishLine.cs
+++ SnowSurfer/Assets/Scripts/FinishLine.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float restartDelay = 1f;
+    [SerializeField] ParticleSystem goalParticle;
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Player"); // avoid error when changing layer's index
         if (collision.gameObject.layer == layerIndex)
         {
             Debug.Log("The player has won");
+            goalParticle.Play();
             Invoke("Restart", restartDelay); // restart the game when player meet finish line
         }
     }
